2e1fff7fad8f8aa81f9a9c5c098f4ddae387404e
Gyslaine Ishimwe <gyslaineishimwe4559@gmail.com>
2026-09-03 16:20:07 +0200

    Update analysis.py
    update the unemployment rate function

--- src/analysis.py
+++ src/analysis.py
@@ -2,4 +2,10 @@ def calculate_rate(enumerator, denominator):
     return (enumerator / denominator) * 100
 
 def calculate_unemployment_rate(unemployed, labour_force):
+    if labour_force <= 0:
+        return None
+
+    if unemployed < 0 or unemployed > labour_force:
+        return None
+
     return calculate_rate(unemployed, labour_force)
