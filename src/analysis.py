eb196ef99fe83ebadc1535ff6a03ea4b677066c3
Gyslaine Ishimwe <gyslaineishimwe4559@gmail.com>
2026-09-03 16:03:22 +0200

    Update analysis.py
    Add a function to calculate unemployment rate

--- src/analysis.py
+++ src/analysis.py
@@ -1,2 +1,5 @@
 def calculate_rate(enumerator, denominator):
     return (enumerator / denominator) * 100
+
+def calculate_unemployment_rate(unemployed, labour_force):
+    return calculate_rate(unemployed, labour_force)
