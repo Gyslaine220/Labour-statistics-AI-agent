970d37b1063a8102a3eb196da605b336feb7120b
Gyslaine Ishimwe <gyslaineishimwe4559@gmail.com>
2026-09-03 16:33:40 +0200

    Update analysis.py
    Add function to calculate LFPR

--- src/analysis.py
+++ src/analysis.py
@@ -1,6 +1,7 @@
 def calculate_rate(enumerator, denominator):
     return (enumerator / denominator) * 100
 
+# function to calculate the unemployment rate
 def calculate_unemployment_rate(unemployed, labour_force):
     if labour_force <= 0:
         return None
@@ -9,3 +10,13 @@ def calculate_unemployment_rate(unemployed, labour_force):
         return None
 
     return calculate_rate(unemployed, labour_force)
+
+#Function to calculate the labour force participation rate (LFPR)
+def calculate_lfpr(labour_force, working_age_population):
+    if working_age_population <= 0:
+        return None
+
+    if labour_force < 0 or labour_force > working_age_population:
+        return None
+
+    return calculate_rate(labour_force, working_age_population)
