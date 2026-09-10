a68a8a0ca2b1d696be2b8d2691031fe1eea5113b
Gyslaine Ishimwe <gyslaineishimwe4559@gmail.com>
2026-09-10 15:40:57 +0200

    Add employment rate question support
    Add employment to population ratio recognition.

--- src/agent.py
+++ src/agent.py
@@ -1,11 +1,4 @@
 from analysis import (
-    calculate_rate,
-    load_data,
-    calculate_lfpr,
-    calculate_unemployment_rate,
-    calculate_employment_rate
-)
-from analysis import (
     load_data,
     calculate_lfpr,
     calculate_unemployment_rate,
@@ -65,15 +58,54 @@ def ask_agent(question):
 
         return f"The unemployment rate is {unemployment_rate:.2f}%."
 
+    # Employment_to_Population Ratio
+    if (
+
+        "employment rate" in question
+
+        or "employment-to-population ratio" in question
+
+        or "employment to population ratio" in question
+
+        or "epr" in question
+
+    ):
+
+        df = load_data("Data/RW_LFS2024.dta")
+
+        working_age_population = df.loc[
+
+            df["wap16"] == 1, "weight2"
+
+        ].sum()
+
+        employed = df.loc[
+
+            df["status1"] == "Employed", "weight2"
+
+        ].sum()
+
+        employment_rate = calculate_employment_rate(
+
+            employed,
+
+            working_age_population
+
+        )
+
+        return (
+
+            f"The Employment-to-Population Ratio is "
+
+            f"{employment_rate:.2f}%."
+
+        )
+
     return "I don't know how to answer that yet."
 
 
-print(ask_agent("What is the LFPR?"))
-print(ask_agent("What is the unemployment rate?"))
 
-# checking whether the agent can answer questions asked differently
+print(ask_agent("What is the employment-to-population ratio?"))
 print(ask_agent("What is the LFPR?"))
-print(ask_agent("What is the labour force participation rate?"))
 print(ask_agent("What is the unemployment rate?"))
-print(ask_agent("Can you tell me the unemployment rate?"))
 print(ask_agent("What is the employment rate?"))
